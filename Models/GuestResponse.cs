9b1b5c9abe4b834699d85f3216ae7ec665eefaba
FallenParadise <JokerChYoutube@mail.ru>
2020-09-07 18:45:38 +0300

    validation

--- Models/GuestResponse.cs
+++ Models/GuestResponse.cs
@@ -1,9 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PartyInvites.Models
 {
     public class GuestResponse {
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]{2,}$", 
+         ErrorMessage = "Characters are not allowed.")]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [Phone]
         public string Phone { get; set; }
+        [Required]
+        [RegularExpression(@"^[0-9]+$", 
+         ErrorMessage = "Characters are not allowed.")]
+        public uint Age { get; set; }
         public bool? WillAttend { get; set; }
     }
 }
